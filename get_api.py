1756e9dce37fcba6163cf6c8301e7e36daa9e883
CaVaiRs <Unlimi.net@mail.ru>
2023-10-24 21:44:38 +0300

    0.1 ver

--- get_api.py
+++ get_api.py
@@ -14,27 +14,31 @@ import json
 
 
 # лицевой счет
-get_ls = 831458
+get_ls = 843575
 # password = input('Введите пароль:')
 
 url = 'https://api.cpx.ru/ask.php'
 payload = {'id':  get_ls}
+try:
+    response = requests.get(url, params=payload)
+    data = response.json()
+    abonent_meta_list = data['abonent']
+    abonent_find = abonent_meta_list[0]
+    ab_id = int(abonent_find['AB_ID'])
+    ab_balanse = abonent_find['ACC_CURRENT_BALANCE']
+    abonent_sevisce_list = data['ct_users']
+    abonent_sevisce_find = abonent_sevisce_list[0]
+    abonent_sevisce_user_in = abonent_sevisce_find['ct_user_services']
+    abonent_sevisce_user_out = abonent_sevisce_user_in[2]
+    ab_lk_password = abonent_sevisce_user_out['USS_PASSWORD']
+except KeyError:
+    pass
+except requests.exceptions.ConnectionError:
+    pass
+except Exception:
+    pass
 
-response = requests.get(url, params=payload)
-data = response.json()
-print(data)
-abonent_meta_list = data['abonent']
-abonent_find = abonent_meta_list[0]
-ab_id = int(abonent_find['AB_ID'])
-ab_balanse = abonent_find['ACC_CURRENT_BALANCE']
-abonent_sevisce_list = data['ct_users']
-abonent_sevisce_find = abonent_sevisce_list[0]
-abonent_sevisce_user_in = abonent_sevisce_find['ct_user_services']
-abonent_sevisce_user_out = abonent_sevisce_user_in[2]
-ab_lk_password = abonent_sevisce_user_out['USS_PASSWORD']
-
-
-print(ab_id, ab_balanse, ab_lk_password)
+# print(ab_id, ab_balanse, ab_lk_password)
 
 
 try:
@@ -54,17 +58,17 @@ try:
         # cursor.execute(
         #     "SELECT version();"
         # )
-        find_select = 'SELECT personal_account FROM user_hotspot WHERE personal_account = %(value)s'
-        params = {'value': ab_id}
+        find_select = 'SELECT personal_account FROM user_hotspot WHERE personal_account = %s'
+        params = (ab_id,)
         cursor.execute(find_select, params)
         result_fetchone = cursor.fetchone()
-        print(result_fetchone)
-        result = result_fetchone[0]
-
-        if result == ab_id:
+        if result_fetchone is not None and ab_lk_password == ab_lk_password:
+            print(ab_lk_password)
             print('Такое уже есть ')
 
-        elif result == None:
+        # elif result_fetchone is not None and ab_lk_password != ab_lk_password:   
+
+        else:
             query = 'INSERT INTO user_hotspot (personal_account, password_account) VALUES (%s, %s);'
             data = (ab_id, ab_lk_password)
             cursor.execute(query, data)
@@ -74,7 +78,7 @@ try:
 
 except Exception as ex:
     print('Error', ex)
-    pass
+    
 
 finally:
     if connection:
